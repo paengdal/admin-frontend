e2ecb01187bd7c7ce693818899e020d18e7b9c55
Hyeongmin Jo <paengdal1@gmail.com>
2025-04-29 16:36:42 +0900

    feat: 댓글 작성/수정/삭제 시 토스트 팝업 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from 'react-hot-toast';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { ROUTES } from './constants/routes';
 import LoginPage from './pages/LoginPage';
@@ -10,20 +11,23 @@ import PostListPage from './pages/posts/PostListPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to={ROUTES.LOGIN} replace />} />
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to={ROUTES.LOGIN} replace />} />
 
-      {/* 회원 관련 */}
-      <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-      <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
-      <Route path={ROUTES.PROFILE_EDIT} element={<ProfileEditPage />} />
+        {/* 회원 관련 */}
+        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+        <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
+        <Route path={ROUTES.PROFILE_EDIT} element={<ProfileEditPage />} />
 
-      {/* 게시글 관련 */}
-      <Route path={ROUTES.POSTS.LIST} element={<PostListPage />} />
-      <Route path={ROUTES.POSTS.CREATE} element={<PostCreatePage />} />
-      <Route path={ROUTES.POSTS.EDIT()} element={<PostEditPage />} />
-      <Route path={ROUTES.POSTS.DETAIL()} element={<PostDetailPage />} />
-    </Routes>
+        {/* 게시글 관련 */}
+        <Route path={ROUTES.POSTS.LIST} element={<PostListPage />} />
+        <Route path={ROUTES.POSTS.CREATE} element={<PostCreatePage />} />
+        <Route path={ROUTES.POSTS.EDIT()} element={<PostEditPage />} />
+        <Route path={ROUTES.POSTS.DETAIL()} element={<PostDetailPage />} />
+      </Routes>
+      <Toaster position="bottom-center" />
+    </>
   );
 }
 
